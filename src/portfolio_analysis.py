5204b4e64d6ca10206d0e94dac9b1720fccb33d7
Mostafa-MMK <shuvrobbaju@gmail.com>
2026-06-13 21:06:54 +0200

    OOP functions added

--- src/portfolio_analysis.py
+++ src/portfolio_analysis.py
@@ -113,3 +113,116 @@ def portfolio_analytics(tickers, risk_free_rate, n_trading_days, confidence_leve
 
     return annual_results, risk_attribution, corr_mat
 
+
+
+
+class Portfolio_Analyis:
+    def __init__(self, tickers, ticker_name, risk_free_rate, start_date, n_trading_days, confidence_level, seed = None, weights = None, port_type = None):
+        self.tickers = tickers
+        self.ticker_name = ticker_name
+        self.risk_free_rate = risk_free_rate
+        self.start_date = start_date
+        self.n_trading_days = n_trading_days
+        self.confidence_level = confidence_level
+        self.weights = weights
+        self.port_type = port_type
+        self.seed = seed
+
+    def Data_Wrangling_Func(self):
+        import pandas as pd
+        import numpy as np
+        from scipy.stats import norm
+        import yfinance as yf
+        rng = np.random.default_rng(seed=self.seed)
+
+
+        # Downloading Tickers
+        df_close = yf.download(tickers=self.tickers, start=self.start_date, auto_adjust=True)['Close']
+        df_close.columns = self.ticker_name
+
+        na_cols_200 = df_close.columns[df_close.isna().sum() > 200]
+        df_close = df_close.drop(columns=na_cols_200)
+
+        log_returns = np.log(df_close / df_close.shift(1)).dropna()
+        n_assets = log_returns.shape[1]
+
+
+        market_caps = []
+        for ticker in self.tickers:
+            t = yf.Ticker(ticker)
+            market_caps.append(t.info['marketCap'])
+        market_caps = np.array(market_caps)
+
+        # Weights Measuring
+        if self.port_type == 'EW':
+            self.weights = np.ones(n_assets) / n_assets
+
+        elif self.port_type == 'MC':
+            self.weights = market_caps / market_caps.sum()
+
+        elif self.port_type == 'RP':
+            self.weights = 1 / log_returns.std().values
+            self.weights /= self.weights.sum()
+
+        elif self.port_type == 'RM':
+            self.weights = rng.random(size=n_assets)
+            self.weights /= self.weights.sum()
+
+        else:
+            if self.weights is None:
+                raise ValueError('Please insert weights!')
+
+            self.weights = np.array(self.weights).flatten()
+            self.weights /= self.weights.sum() # for ensuring sum is 1.0      
+
+        # Calculating Portfolio
+        corr_mat = log_returns.corr()
+        cov_mat = log_returns.cov()
+
+        port_returns = log_returns @ self.weights
+        port_std = np.sqrt(self.weights.T @ cov_mat @ self.weights)
+
+
+        # Annualized Portfolio Params
+        annual_port_returns = (log_returns @ self.weights).mean() * self.n_trading_days
+        annual_port_std = np.sqrt((self.weights.T @ cov_mat @ self.weights) * self.n_trading_days)
+        annual_sharpe_ratio = (annual_port_returns - self.risk_free_rate) / annual_port_std
+        daily_VaR = np.percentile(port_returns, (1 - self.confidence_level) * 100)
+        daily_ES = port_returns[port_returns <= daily_VaR].mean()
+        annual_VaR = daily_VaR * np.sqrt(self.n_trading_days)
+        annual_ES = daily_ES * np.sqrt(self.n_trading_days)
+
+        annual_results = pd.DataFrame({
+            'Annual Port Returns': [annual_port_returns],
+            'Annual Port Std.': [annual_port_std],
+            'Annual Sharpe Ratio': [annual_sharpe_ratio],
+            'Annual VaR': [annual_VaR],
+            'Annual ES': [annual_ES]
+        })
+
+
+        # Risk Breakdown
+        avg_daily_returns = log_returns.mean()
+        avg_daily_std = log_returns.std()
+        daily_risk_free_rate = self.risk_free_rate / self.n_trading_days
+        avg_sharpe_ratios = ((log_returns - daily_risk_free_rate) / log_returns.std()).mean()
+
+        marginal_risk_contribution = (cov_mat @ self.weights) / np.sqrt(self.weights.T @ cov_mat @ self.weights)
+        component_risk_contribution = self.weights * marginal_risk_contribution
+        pct_risk_contribution = component_risk_contribution / component_risk_contribution.sum()
+
+        risk_breakdown = pd.DataFrame({
+            'Asset': log_returns.columns,
+            'Weight': self.weights,
+            'Avg. Daily Returns': avg_daily_returns,
+            'Avg. Daily Std.': avg_daily_std,
+            'Avg. Sharpe Ratio': avg_sharpe_ratios,
+            'Marginal Risk Contribution': marginal_risk_contribution,
+            'component Risk Contribution': component_risk_contribution,
+            '% of Total Risk': pct_risk_contribution
+        })
+        risk_breakdown = risk_breakdown.sort_values('% of Total Risk', ascending=False)
+
+
+        return risk_breakdown, annual_results
+
